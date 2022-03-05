05dac990cc63661816e61aa45bc4960d41e3a9c7
tusharjadhav62040 <tusharjadhav62040@gmail.com>
2022-03-05 10:15:11 +0530

    First Commit

---
+++ Java_Git_Integration/src/pack1/FirstCode.java
@@ -0,0 +1,23 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class FirstCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int z, y, temp;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number 1");
+		z = sc.nextInt();
+		System.out.println("Enter Number 2");
+		y = sc.nextInt();
+		System.out.println("Before Swapping\nz = "+z+"\ny = "+y);
+		temp = z;   
+		z = y;
+		y = temp;
+		System.out.println("After Swapping\nz = "+z+"\ny = "+y);
+
+	}
+
+}
